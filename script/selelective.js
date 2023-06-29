4abba3c86a9351a85e2722f9a5c831c90ed4aa7e
suyog200 <suygchari@gmail.com>
2023-06-29 11:46:13 +0530

    compeleted all containers

--- script/selelective.js
+++ script/selelective.js
@@ -3,9 +3,13 @@
 const navLinks = document.querySelectorAll('.nav__links');
 const infoBtn = document.querySelectorAll('.more__info--btn');
 const cardContainer = document.querySelector('.cards__container');
-const selectionContainer = document.querySelector('.selection__container');
 const selectSubjectBtn = document.querySelector('.select-btn');
+const backHomeBtn = document.querySelector('.bck-home-btn');
+const confBtn = document.querySelector('.conf-btn');
+const cancelBtn = document.querySelector('.cancel-btn');
+const selectionContainer = document.querySelector('.selection__container');
 const confrimationContainer = document.querySelector('.confrimation__container');
+const subjectSelectedContainer = document.querySelector('.subject--selected__container');
 
 
 
@@ -15,11 +19,12 @@ navLinks.forEach((lnk,i) => lnk.addEventListener('click', function() {
     if(i == 0){
         cardContainer.classList.remove('unactive');
         confrimationContainer.classList.add('unactive');
+        subjectSelectedContainer.classList.add('unactive');
     }
     else {
         return
     }
-}))
+}));
 
 infoBtn.forEach((btn) => btn.addEventListener('click', function(e) {
     e.preventDefault();
@@ -31,5 +36,23 @@ selectSubjectBtn.addEventListener('click', function(e) {
     e.preventDefault();
     confrimationContainer.classList.remove('unactive');
     selectionContainer.classList.add('unactive');
+});
 
-})
+confBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    subjectSelectedContainer.classList.remove('unactive');
+    confrimationContainer.classList.add('unactive');
+});
+
+cancelBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    confrimationContainer.classList.add('unactive');
+    selectionContainer.classList.remove('unactive')
+});
+
+backHomeBtn.addEventListener('click', function(e) {
+    cardContainer.classList.remove('unactive');
+    confrimationContainer.classList.add('unactive');
+    subjectSelectedContainer.classList.add('unactive');
+    selectionContainer.classList.add('unactive');
+});
